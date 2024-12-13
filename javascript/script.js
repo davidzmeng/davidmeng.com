ae50ba3c65f9b5b4598f414379a52d4f589312ea
David Meng <david.zl.meng@gmail.com>
2024-12-12 21:58:18 -0500

    added code to js

--- javascript/script.js
+++ javascript/script.js
@@ -3,3 +3,14 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'G-X3WXRWZE6N');
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('a').forEach(function(link) {
+      link.addEventListener('click', function() {
+        gtag('event', 'click', {
+          'event_category': 'Links',
+          'event_label': link.href
+        });
+      });
+    });
+  });
